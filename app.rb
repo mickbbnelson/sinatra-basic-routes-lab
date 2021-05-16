502fd3019fe8b8e385a48ea838224e357d5f6ae9
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-05-16 09:09:21 -0700

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get "/name" do 
+        "My name is Mick"
+    end
+
+    get "/hometown" do 
+        "My hometown is Hastings"
+    end
+
+    get "/favorite-song" do 
+        "My favorite song is Inner City Blues"
+    end
 end
